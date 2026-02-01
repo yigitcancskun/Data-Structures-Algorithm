90d94c11747ce93731b467d0c0f9cd0d078f3a92
yigitcancskun <yigitcancskun@gmail.com>
2026-02-01 16:14:19 +0300

    Implement pop method for LinkedList class and update test cases

--- Node_linkedlist_relationship.py
+++ Node_linkedlist_relationship.py
@@ -28,11 +28,26 @@ class LinkedList:
         self.length += 1
         return True
 
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        pre = self.head
+        while temp.next is not None:
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
 
 
 
 my_linked_list = LinkedList(1)
 my_linked_list.append(2)
-my_linked_list.append(3)
-my_linked_list.append(4)
-my_linked_list.print_list()
+print(my_linked_list.pop())
+print(my_linked_list.pop())
+print(my_linked_list.pop())
