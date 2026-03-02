34ddb154fe62cfe42780e8de18dee07c6f1c7f12
yigitcancskun <yigitcancskun@gmail.com>
2026-03-02 11:39:22 +0300

    added graphs

---
+++ graphs.py
@@ -0,0 +1,37 @@
+# only graphs have weighted edges
+# adjacency matrix = bitişiklik matrisi
+class Graph:
+    def __init__(self):
+        self.adj_list = {}
+    
+    def add_vertex(self,vertex):
+        if vertex not in self.adj_list.keys():
+            self.adj_list[vertex] = []
+            return True
+        return False
+    
+    def add_edge(self,v1,v2):
+        if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
+            self.adj_list[v1].append(v2)
+            self.adj_list[v2].append(v1)
+            return True
+        return False
+    
+    def remove_edge(self,v1,v2):
+        if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
+            try:
+                self.adj_list[v1].remove(v2)
+                self.adj_list[v2].remove(v1)
+            except ValueError:
+                pass
+            return True
+        return False
+    
+    def remove_vertex(self, vertex):
+        # we need to remove first edges before vertex
+        if vertex in self.adj_list.keys():
+            for other_vertex in self.adj_list[vertex]:
+                self.adj_list[other_vertex].remove(vertex)
+            del self.adj_list[vertex]
+            return True
+        return False
